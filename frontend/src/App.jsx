916b66dcde2d3da13f185345ca5563bd3cd15948
ollok <lhjbg0821@gmail.com>
2023-06-15 23:28:17 +0900

    header, detail 컴포넌트 추가

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Detail from "./components/Detail";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="">
+      <Header></Header>
+      <Detail></Detail>
+    </div>
+  );
+}
+
+export default App;
