b38da032eb84830ecce4da64905539c5f43cd8a5
ollok <lhjbg0821@gmail.com>
2023-06-16 05:24:43 +0900

    상품 상세 페이지로 이동할 수 있는 detailbox

---
+++ frontend/src/components/DetailBox.jsx
@@ -0,0 +1,10 @@
+const DetailBox = () => {
+  return (
+    <div className="bg-gray-100 w-96 h-96 flex flex-col justify-between items-center">
+      <img className="w-96 h-96" />
+      <div className="bg-gray-700 text-gray-50 w-96 py-4">선물 이름</div>
+    </div>
+  );
+};
+
+export default DetailBox;
