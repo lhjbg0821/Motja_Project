fc14f079fe3da5ad37580669d6ceea2a1e58d8d6
ollok <lhjbg0821@gmail.com>
2023-06-20 23:19:03 +0900

    main page 생성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Detail from "./components/Detail";
 import Header from "./components/Header";
+import Main from "./pages/main";
 
 function App() {
   return (
     <div className="">
       <Header></Header>
+      <Main></Main>
       <Detail></Detail>
     </div>
   );
