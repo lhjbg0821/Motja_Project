e6679ec381f4ca503f0cac586bd06dd426b6561c
ollok <lhjbg0821@gmail.com>
2023-06-24 09:18:35 +0900

    app.jsx 수정 (footer 위로 수정/ 다시 수정 예정

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Main from "./pages/main";
+import Footer from "./components/Footer";
+
 function App() {
-  return <div className="bg-red-100">Hello, React!</div>;
+  return (
+    <div className="bg-red-300">
+      <BrowserRouter>
+        <Header />
+        <Footer />
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
