91383f6f4fb7d73b868d4ca7ba0b994fa7cd5946
ollok <lhjbg0821@gmail.com>
2023-06-24 09:16:54 +0900

    Header 수정

---
+++ frontend2/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="bg-purple-400 flex justify-between px-36 py-12">
+      <img className="" alt="로고이미지"></img>
+      <button>My Page</button>
+    </div>
+  );
+};
+
+export default Header;
