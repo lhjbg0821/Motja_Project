048e557151da6c804108bd8db473d635baba0942
ollok <lhjbg0821@gmail.com>
2023-06-30 14:33:13 +0900

    public 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Detail from "./components/Detail";
 import Header from "./components/Header";
 import Main from "./pages/main";
+import Present_Detail from "./pages/present_detail";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="">
+      <div>
         <Header></Header>
-        <Routes>
+        {/* <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/detail" element={<Detail />} />
-        </Routes>
+          <Route path="/detail" element={<Present_Detail />} />
+        </Routes> */}
+        <Footer />
       </div>
     </BrowserRouter>
   );
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,23 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <Box as="footer" py={4} bg="gray.200">
+      <Flex align="center" justify="center">
+        <Text fontSize="sm" fontWeight="bold">
+          프로젝트 팀 이름
+        </Text>
+      </Flex>
+      <Box mt={2}>
+        <Flex align="center" justify="center">
+          <Text fontSize="xs" color="gray.500">
+            © 2023 Team Motja. All rights reserved. Reproduction, distribution,
+            or modification without authorization is strictly prohibited.
+          </Text>
+        </Flex>
+      </Box>
+    </Box>
+  );
 };
 
 export default Footer;
